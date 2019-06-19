cf897251b9bc8614c655266465ed99cb0b437d62
Clint Bellanger <clintbellanger@gmail.com>
2019-06-19 11:16:31 -0500

    ORCID js rewritten from fetch to XMLHttpRequest Added wrapper to display Profile links in a simpler way

--- orcid.js
+++ orcid.js
@@ -1,205 +1,155 @@
 /**
- * Created by michaelcrabb on 05/03/2017.
+ * Based on orcid.js by michaelcrabb 05/03/2017.
+ * Modified by clintbellanger on 05/22/2019 for Auburn University Libraries
  */
 
+// display the ORCID link and create list of works
+function showORCIDProfile(orcidID, linkElementID, worksElementID) {
+  
+  if (orcidID == "XXXX-XXXX-XXXX-XXXX") {
+    console.log("Notice: replace XXXX-XXXX-XXXX-XXXX with your ORCID ID in the embed code");
+    return;    
+  }
+  
+  createORCIDLink(orcidID, linkElementID);
+  
+  document.addEventListener("DOMContentLoaded", function(){
+    createORCIDWorks(orcidID, worksElementID);
+  });
+    
+}
 
+
+// formatting for ORCID main profile link
+function createORCIDLink(orcidID, linkElementID) {
+
+  var output = "";
+  output += '<div itemscope itemtype="https://schema.org/Person">';
+  output += '<a itemprop="sameAs" content="https://orcid.org/' + orcidID + '" href="https://orcid.org/' + orcidID + '" target="orcid.widget" rel="noopener noreferrer" style="vertical-align:top;">';
+  output += '<img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" style="width:1em;margin-right:.5em;" alt="ORCID iD icon">orcid.org/' + orcidID + '</a>';
+  output += '</div>';
+  
+  document.getElementById(linkElementID).innerHTML = output;
+        
+}
+
+// TEMP transitional
 function createORCIDProfile(orcidID, elementID) {
+    createORCIDWorks(orcidID, elementID);
+}
+
+
+// populate list of works by this ORCID person
+function createORCIDWorks(orcidID, elementID) {
 
   var ORCIDLink = "https://pub.orcid.org/v2.0/" + orcidID + "/works";
+  
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
 
-  fetch(ORCIDLink,
+      var xmldata = xhr.responseXML;
+     
+      //DEBUG!
+      //console.log(xmldata);
 
-      {
-        headers: {
-          "Accept": "application/orcid+json"
-        }
-      })
-    .then(
-      function(response) {
-        if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-          return;
-        }
-
-        // Examine the text in the response
-        response.json().then(function(data) {
-
-          ////DEBUG!
-          // console.log(data);
-
-          var output = "";
-          for (var i in data.group) {
-            //PAPER NAME
-            if (data.group[i]["work-summary"]["0"].title.title.value != null) {
-              var publicationName = data.group[i]["work-summary"]["0"].title.title.value;
-            }
-
-
-            //PUBLICATION YEAR
-            if (data.group[i]["work-summary"]["0"]["publication-date"] != null) {
-              var publicationYear = data.group[i]["work-summary"]["0"]["publication-date"].year.value;
-            } else {
-              var publicationYear = "";
-            }
-
-            //DOI REFERENCE
-            if (data.group[i]["external-ids"]["external-id"]["length"] != 0) {
-              for (var j in data.group[i]["external-ids"]["external-id"]) {
-                if (data.group[i]["external-ids"]["external-id"][j]["external-id-type"] == 'doi') {
-                  var doiReference = data.group[i]["external-ids"]["external-id"][j]["external-id-value"];
-                  break;
-                }
-              }
-            } else {
-              var doiReference = "";
-            }
-
-            //JOURNAL NAME
-            var putcode = data.group[i]["work-summary"]["0"]["put-code"];
-            //console.log(journalTitle);
-
-            output += "<p><span id='publication_" + i + "'><strong>" + publicationName + "</strong>";
-            output += " (" + publicationYear + ") </em></span>";
-            output += " <a href='https://doi.org/" + doiReference + "'> " + doiReference + "</a></p>";
-            getJournalTitle(orcidID, putcode, i);
-
-          }
-
-          document.getElementById(elementID).innerHTML = output;
-        });
+      var output = "";
+      
+      var groups = xmldata.getElementsByTagName("activities:group");
+      
+      // first create the blank Work elements on the page
+      for (var i=0; i < groups.length; i++) {        
+        var putcode = groups[i].getElementsByTagName("work:work-summary")[0].getAttribute("put-code");
+        output += "<div id=\"work_" + putcode + "\" class=\"work\" ></div>\n";
       }
-    )
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
+      document.getElementById(elementID).innerHTML = output;
+      
+      // next get the Work details asynchronously
+      for (var i=0; i < groups.length; i++) {        
+        var putcode = groups[i].getElementsByTagName("work:work-summary")[0].getAttribute("put-code");
+        getWorkInformation(orcidID, putcode, "work_" + putcode);
+      }
+     
+    }
+  };
+  xhr.open("GET", ORCIDLink, true);
+  xhr.send();
+
 }
 
-function getJournalTitle(orcidID, journalID, i) {
-  var ORCIDLink = "https://pub.orcid.org/v2.0/" + orcidID + "/work/" + journalID;
-  fetch(ORCIDLink, {
-      headers: {
-        "Accept": "application/orcid+json"
+function getWorkInformation(orcidID, putcode, elementID) {
+  var ORCIDLink = "https://pub.orcid.org/v2.0/" + orcidID + "/work/" + putcode;
+  
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+      var xmldata = xhr.responseXML;
+     
+      //DEBUG!
+      //console.log(xmldata);
+
+      var output = "";
+            
+      // citations can be preformatted or in structured bibtex    
+      var citation_raw = xmldata.getElementsByTagName("work:citation-value")[0].textContent;
+      var citation;
+      
+      var bibtex_opener = "@article";
+      
+      if (citation_raw.substr(0, bibtex_opener.length) === bibtex_opener) { // bibtex style
+        citation = formatBibTeXCitation(citation_raw);
       }
-    })
-    .then(
-      function(response) {
-        if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-          return;
-        }
-        response.json().then(function(data) {
-          if (data["journal-title"] != null) {
-            var output = data["journal-title"].value;
-            document.getElementById("publication_" + i).innerHTML = document.getElementById("publication_" + i).innerHTML + output;
-          }
-        });
+      else { // preformatted style
+        citation = citation_raw;  
       }
-    )
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
+      
+      // here we use the base <work:url>, but
+      // note there are external IDs that can form alternate URLs
+      var element_url = xmldata.getElementsByTagName("work:url");
+      var work_url = "";
+      if (element_url && element_url[0] && element_url[0].textContent) {
+        work_url = element_url[0].textContent;  
+      }
+
+      output = citation;
+      output += "<br />\n";
+      
+      if (work_url) {
+        output += "<a href=\"" + work_url + "\" target=\"_blank\">" + work_url; + "</a>";
+      }
+      
+      document.getElementById(elementID).innerHTML = output;
+     
+    }
+  };
+  xhr.open("GET", ORCIDLink, true);
+  xhr.send();  
 }
 
-function createORCIDFundingProfile(orcidID, elementID) {
-
-  var ORCIDLink = "https://pub.orcid.org/v2.0/" + orcidID + "/fundings";
-
-  fetch(ORCIDLink,
-
-      {
-        headers: {
-          "Accept": "application/orcid+json"
-        }
-      })
-    .then(
-      function(response) {
-        if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-          return;
-        }
-        response.json().then(function(data) {
-
-          ////DEBUG!
-          // console.log(data);
-
-          var output = "";
-          for (var i in data.group) {
-            //GET PUT CODES
-            var putcode = data.group[i]["funding-summary"]["0"]["put-code"];
-            getFundingInformation(putcode, orcidID, elementID)
-          }
-
-          document.getElementById(elementID).innerHTML = output;
-        });
-      }
-    )
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
-}
-
-function getFundingInformation(putcode, orcidID, elementID) {
-  var ORCIDLink = "https://pub.orcid.org/v2.0/" + orcidID + "/funding/" + putcode;
-
-  fetch(ORCIDLink,
-
-      {
-        headers: {
-          "Accept": "application/orcid+json"
-        }
-      })
-    .then(
-      function(response) {
-        if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-          return;
-        }
-        response.json().then(function(data) {
-
-          ////DEBUG!
-          console.log(data);
-
-          if (data["title"] != null) {
-            var fundingTitle = data["title"]["title"].value;
-          } else {
-            var fundingTitle = "";
-          }
-
-          if (data["organization-defined-type"].value != null) {
-            var fundingType = data["organization-defined-type"].value;
-          } else {
-            var fundingType = "";
-          }
-
-          if (data["organization"]["name"] != null) {
-            var fundingBody = data["organization"]["name"];
-          } else {
-            var fundingBody = "";
-          }
-
-          if (data["start-date"]["year"] != null) {
-            var startDate = data["start-date"]["year"].value;
-          } else {
-            var startDate = "";
-          }
-
-          var output = "<p>";
-          output += "<strong>" + fundingTitle + "</strong> ";
-          output += "(" + startDate + "), "
-          output += fundingBody + " <em>(" + fundingType + ")</em>";
-          output += "</p>";
-
-          document.getElementById(elementID).innerHTML = output + document.getElementById(elementID).innerHTML;
-
-        });
-
-      }
-    )
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
+function formatBibTeXCitation(bibtex_string) {
+    
+  // using https://github.com/ORCID/bibtexParseJs
+  var bibtex = bibtexParse.toJSON(bibtex_string)[0].entryTags;
+  
+  //DEBUG!
+  //console.log(bibtex);
+  
+  // using APA-like formatting 
+  var output = "";
+  output += bibtex.author + " ";
+  output += "(" + bibtex.year + ") ";
+  output += bibtex.title + ". ";
+  output += "<i>" + bibtex.journal + "</i>, ";  
+  if (bibtex.volume) {
+    output += bibtex.volume + " ";
+  }  
+  if (bibtex.number) {
+    output += "(" + bibtex.number + "), ";
+  }
+  if (bibtex.pages) {
+    output += bibtex.pages + ".";  
+  }
+  
+  return output;
 }
